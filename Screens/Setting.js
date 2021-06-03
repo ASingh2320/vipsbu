3894370e1af45308393a1082e028c6c90daab4da
Sammi Lin <64671027+sxmmi@users.noreply.github.com>
2021-06-03 14:51:30 -0400

    Settings screen update
    added four new buttons: Manage Account, Dark Mode, User Feedback, Logout

--- Screens/Setting.js
+++ Screens/Setting.js
@@ -2,12 +2,43 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect }  from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-const Setting = (props) => {
+export default function Setting() {
+    const [showLogin, toggleLogin] = useState(false); 
+    const [darkMode, toggleDarkMode] = useState(false);
+    const [manageAccount, toggleManageAccount] = useState(false);
+    const [userFeedback, toggleUserFeedback] = useState(false);
+
+    const setManageAccount = () => {
+        toggleLogin(false);
+        toggleFeed(false);
+        toggleManageAccount(!manageAccount)
+    }
+
+    const setDarkMode = () => {
+        toggleLogin(false);
+        toggleFeed(!darkMode);
+        toggleManageAccount(false)
+    }
+
+    const setUserFeedback = () => {
+        toggleLogin(false);
+        toggleFeed(false);
+        toggleManageAccount(false);
+        toggleUserFeedback(!userFeedback);
+    }
+
+    const setShowLogin = () => {
+        toggleLogin(false);
+        toggleDarkMode(false);
+        toggleManageAccount(false)
+    }
+
     return (
         <View>
-            <Text>Settings</Text>
+            <Button title="Manage Account" onPress={setManageAccount}/>
+            <Button title="Dark Mode" onPress={setDarkMode}/>
+            <Button title="User Feedback" onPress={setUserFeedback}/>
+            <Button title="Logout" onPress={setShowLogin}/>
         </View>
-    );
-};
-
-export default Setting;
+    )
+}
