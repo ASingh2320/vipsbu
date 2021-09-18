00cac819a745d42f9235bf9e30648062af1fbfec
EricChen-01 <76068901+EricChen-01@users.noreply.github.com>
2021-09-18 03:39:18 -0400

    added comments in MenuStack.js for clarity

--- components/MenuStack.js
+++ components/MenuStack.js
@@ -4,28 +4,33 @@ import { Entypo, MaterialCommunityIcons, Feather, Ionicons, AntDesign} from '@ex
 
 // MenuStack: returns a view that is a horizontal stack with Profile, Bug, Map, Settings, and Exit options.
 // Parameters: toggleMenu -> used to close the menu.
+
 const MenuStack = ({toggleMenu, gotoSettings, gotoMap, gotoFeed, screenName}) => {
     return (
       <View style={styles.container}>
-      
+    
+    {/* if screen is 'login' then do not load the login icon */}
     {(screenName != 'login') && 
       <TouchableOpacity style={styles.button}>
         <MaterialCommunityIcons name="face-profile" size={24} color="black" />
       </TouchableOpacity>
     } 
 
+    {/* if screen is 'bug' then do not load the bug icon */}
     {(screenName != 'bug') && 
       <TouchableOpacity style={styles.button}>
         <Ionicons name="ios-bug" size={24} color="black" onPress={gotoFeed}/>
       </TouchableOpacity>
     }
     
+    {/* if screen is 'map' then do not load the map icon */}
     {(screenName != 'map') &&
       <TouchableOpacity style={styles.button}>
         <Ionicons name="ios-map" size={24} color="black" onPress={gotoMap}/>
       </TouchableOpacity>
     }
 
+    {/* if screen is 'settings' then do not load the settings icon */}
     {(screenName != 'settings') &&
       <TouchableOpacity style={styles.button}>
         <Ionicons name="ios-settings" size={24} color="black" onPress={gotoSettings}/>
@@ -40,7 +45,7 @@ const MenuStack = ({toggleMenu, gotoSettings, gotoMap, gotoFeed, screenName}) =>
     );
 };
 
-
+// styling
 const styles = StyleSheet.create({
     container: {
       flexDirection: 'row'
