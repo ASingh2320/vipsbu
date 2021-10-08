04a33462173dc926b814b935f60e0607d0e6b86b
Sammi Lin <64671027+sxmmi@users.noreply.github.com>
2021-10-08 00:38:46 -0400

    Removed login, user feedback, and manage account. Added font size

--- Screens/Setting.js
+++ Screens/Setting.js
@@ -4,42 +4,23 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function Setting() {
     
-    const [showLogin, toggleLogin] = useState(false); 
+    const [fontSize, toggleFontSize] = useState(false); 
     const [darkMode, toggleDarkMode] = useState(false);
-    const [manageAccount, toggleManageAccount] = useState(false);
-    const [userFeedback, toggleUserFeedback] = useState(false);
 
-    const setManageAccount = () => {
-        toggleLogin(false);
-        toggleFeed(false);
-        toggleManageAccount(!manageAccount)
+    const setFontSize = () => {
+        toggleFontSize(!fontSize)
+        toggleDarkMode(false);
     }
 
     const setDarkMode = () => {
-        toggleLogin(false);
+        toggleFontSize(false);
         toggleFeed(!darkMode);
-        toggleManageAccount(false)
-    }
-
-    const setUserFeedback = () => {
-        toggleLogin(false);
-        toggleFeed(false);
-        toggleManageAccount(false);
-        toggleUserFeedback(!userFeedback);
-    }
-
-    const setShowLogin = () => {
-        toggleLogin(false);
-        toggleDarkMode(false);
-        toggleManageAccount(false)
     }
 
     return (
         <View>
-            <Button title="Manage Account" onPress={setManageAccount}/>
             <Button title="Dark Mode" onPress={setDarkMode}/>
-            <Button title="User Feedback" onPress={setUserFeedback}/>
-            <Button title="Logout" onPress={setShowLogin}/>
+            <Button title="Font Size" onPress={setFontSize}/>
         </View>
     )
     
